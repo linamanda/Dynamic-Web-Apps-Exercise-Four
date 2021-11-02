ec60e6c08e97939e04f5408524da6847c1f71a53
Amanda Lin <amanda.lin.aut@gmail.com>
2021-11-02 08:59:47 -0400

    Start setting up Firebase

--- index.js
+++ index.js
@@ -1,16 +1,37 @@
+// Initiate express
 const express = require("express");
 const app = express();
 
+// Setting port - dynamically with heroku
 const port = process.env.PORT || 4000;
 
+// Import Firebase
+const firebase = require("firebase/app");
+
+// Get configuration object so we can communicate with Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyCt1fWN08W3OTDGOGkjLlNTjHHckCrXa9M",
+  authDomain: "exercise-four-3b037.firebaseapp.com",
+  projectId: "exercise-four-3b037",
+  storageBucket: "exercise-four-3b037.appspot.com",
+  messagingSenderId: "775483658353",
+  appId: "1:775483658353:web:21eee8e642f79146074bd0",
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// Routes for directing user to correct place
 const indexRoute = require("./routes/index.js");
 const articleRoute = require("./routes/article.js");
 const createArticleRoute = require("./routes/createArticle.js");
 
+// Tell exoress to use routes
 app.use("/", indexRoute);
 app.use("/article", articleRoute);
 app.use("/create", createArticleRoute);
 
+// Listen for requests on the port - if a user vists then send a result
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,23 @@
 const express = require("express");
-const { builtinModules } = require("module");
-
+// Middleware to allow for routing on the node server
 const router = express.Router();
 
-// TODO: Hook up firebase, get all articles
+// Require Firebase
+const firebase = require("firebase/firestore/lite");
+
+// Initialize Firestore Database
+const db = firebase.getFirestore();
+// Reference to the blogposts collection
+const blogposts = db.collection("blogposts"); // fix
 
 // Get all articles from firebase
 router.get("/", (req, res) => {
-  res.send(`
-  <h1>All Articles</h1>
-  <p>Articles coming soon... need to set up firebase</p>
-  `);
+  // Create empty array
+  const blogpostsArray = [];
+  // Get blogposts JSON from Firebase...
+
+  // Push document from blogposts into the blogposts array...
+  res.send(blogpostsArray);
 });
 
 module.exports = router;
