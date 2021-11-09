7df522b24eb5b0009eb321c0f9bf89b80c337461
Amanda Lin <amanda.lin.aut@gmail.com>
2021-11-09 09:12:14 -0500

    Complete form and Firestore connection

--- routes/createArticle.js
+++ routes/createArticle.js
@@ -2,27 +2,78 @@ const express = require("express");
 
 const router = express.Router();
 
-// TODO: hook up to firebase to allow for a POST request to create a single article
+//Create form for submitting...
+const form = `
+  <h1>Create Article</h1>
+  <form action="/create/submit">
+    <div style="
+      display: flex;
+      flex-direction: column;
+      margin-bottom: 20px;
+      max-width: 325px;
+    ">
+      <label for="articleTitle">
+        Article Title
+      </label>
+      <input type="text" name="articleTitle" placeholder="type article title..." />
 
+      <label for="articleText">
+        Article Text
+      </label>
+      <input type="text" name="articleText" placeholder="type article text..." />
+
+      <label for="author">
+        Author
+      </label>
+      <input type="text" name="author" placeholder="author name..." />
+    </div>
+    <button type="submit">Submit Article</button>
+  </form>
+`;
+
+// Require Firebase
+const firestore = require("firebase/firestore");
+
+// Reference DB
+const db = firestore.getFirestore();
+
+// Serves web from for users
 router.get("/", (req, res) => {
-  res.send(`
-    <h1>Create Article</h1>
+  res.send(form);
+});
 
-    <form>
-      <div style="
-        display: flex;
-        flex-direction: column;
-        margin-bottom: 20px;
-        max-width: 325px;
-      ">
-        <label for="articleTitle">
-          Article Title
-        </label>
-        <input type="text" name="articleTitle" placeholder="type article title..." />
-      </div>
-      <button type="submit">Submit Article</button>
-    </form>
-    `);
+// API Endpoint for submitting data through our form
+router.get("/submit", (req, res) => {
+  const queryParams = req.query; // Query params from URL
+  const title = queryParams.articleTitle;
+  const text = queryParams.articleText;
+  const author = queryParams.author;
+
+  // Create ID from title
+  const idFromTitle = title.replace(/\s+/g, "-").toLowerCase();
+  // Submit post to Firebase
+  const setBlogPost = firestore.setDoc(
+    firestore.doc(db, "blogposts", idFromTitle),
+    {
+      title,
+      text,
+      author,
+    }
+  );
+
+  setBlogPost
+    .then((reponse) => {
+      // If successful, send correct message
+      res.send(`
+        <h1>Submission Successful!</h1>
+        <p><a href="/create">Add Another Post</a></p>
+      `);
+    })
+    .catch((error) => {
+      // If failure, send correct message
+      console.warn(error);
+      res.send(`Error Submitting: ${error.toString()}`);
+    });
 });
 
 module.exports = router;
